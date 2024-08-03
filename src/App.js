d90b42bd123384a2399257e4eb3dd9d0e06633ef
Mariyah <mariyahjsola@gmail.com>
2024-08-03 20:01:49 +0100

    Added Login and Register Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Landing";
