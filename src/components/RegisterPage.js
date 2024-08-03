59df081772f43a10325ee13ed4cc8fda85ca7ea2
Mariyah <mariyahjsola@gmail.com>
2024-08-03 18:28:41 +0100

    Register Page completed

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Register from "./Register";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h2 className="RegisterWelcome">Register to become a member</h2>
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
