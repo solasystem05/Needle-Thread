5691b46822277bebbdcdddc7ca11603cf345a079
Mariyah <mariyahjsola@gmail.com>
2024-08-04 11:38:53 +0100

    reducers updated for shipping cost

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,19 +1,26 @@
 import { createReducer } from "@reduxjs/toolkit";
-import { addProduct } from "./actions";
+import { addProduct, addShippingCost } from "./actions";
 
 const initialState = {
   totalPrice: 0,
   purchasedProducts: [],
   isTotalPriceVisible: false,
+  shippingCost: 0,
 };
 
 const rootReducer = createReducer(initialState, (builder) => {
-  builder.addCase(addProduct, (state, action) => {
-    const price = parseFloat(action.payload.Price.replace("£", ""));
-    state.totalPrice += price;
-    state.purchasedProducts.push(action.payload);
-    state.isTotalPriceVisible = true;
-  });
+  builder
+    .addCase(addProduct, (state, action) => {
+      const price = parseFloat(action.payload.Price.replace("£", ""));
+
+      state.totalPrice += price;
+
+      state.purchasedProducts.push(action.payload);
+      state.isTotalPriceVisible = true;
+    })
+    .addCase(addShippingCost, (state, action) => {
+      state.shippingCost += action.payload; //shipping cost seperate
+    });
 });
 
 export default rootReducer;
