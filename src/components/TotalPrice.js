b0e6791c2804a82d14a74de4df4925c20ccc56c5
Mariyah <mariyahjsola@gmail.com>
2024-08-04 09:50:05 +0100

    Total Price component for pages and cart

---
+++ src/components/TotalPrice.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TotalPrice = ({ totalPrice, isVisible }) => {
+  return (
+    //box to stay in top right
+    <div
+      style={{
+        display: isVisible ? "flex" : "none",
+        position: "fixed",
+        bottom: "20px",
+        right: "10px",
+        backgroundColor: "#a4a4a0",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "5px",
+        borderRadius: "5px",
+        zIndex: 1000, //overlap
+      }}
+    >
+      <p className="totalbox">Total price: £{totalPrice.toFixed(2)}</p>
+    </div>
+  );
+};
+
+export default TotalPrice;
